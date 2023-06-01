4c2041e5b86450ee9dcedce3abec886bfacc0899
claireWei <claire610323@gmail.com>
2023-06-01 15:03:47 -0700

    Navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,8 +1,107 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { HomeIcon } from '../icons/index'
+import Toast, { ToastProps } from '../toast/Toast'
+import newRequest from '../../utils/newRequest'
+import './Navbar.scss'
 
 const Navbar: FC = () => {
+  const [active, setActive] = useState(false)
+  const [open, setOpen] = useState(false)
+
+  const { pathname } = useLocation()
+
+  const [toastConfig, setToastConfig] = useState<ToastProps>({
+    message: '',
+    isVisible: false
+  })
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', isActive)
+    return () => {
+      window.removeEventListener('scroll', isActive)
+    }
+  }, [])
+  const navigate = useNavigate()
+
+  let currentUser
+  const storageUser = localStorage.getItem('currentUser')
+  if (storageUser) {
+    currentUser = JSON.parse(storageUser)
+  }
+
+  const handleLogout = async () => {
+    try {
+      await newRequest.post('/auth/logout')
+      localStorage.setItem('currentUser', '')
+      navigate('/')
+    } catch (error) {
+      setToastConfig({
+        message: `Error: ${error}`,
+        isVisible: true,
+        type: 'error'
+      })
+    }
+  }
+
   return (
-    <div>Navbar</div>
+    <>
+      <Toast
+        isVisible={toastConfig.isVisible}
+        message={toastConfig.message}
+        type={toastConfig.type}
+        onHide={() => setToastConfig({isVisible: false, message: ''})}
+      />
+      <div className={active || pathname !== '/' ? 'navbar active' : 'navbar'}>
+        <div className='container flex-between'>
+          <div className='logo' onClick={() => navigate('/')}>
+            <HomeIcon />
+          </div>
+          <div className='links'>
+            {currentUser ? (
+              <div className='user flex-center cursor-pointer' onClick={() => setOpen(!open)}>
+                <img src={currentUser.img || '/img/noavatar.jpg'} alt='' />
+                <span>{currentUser?.username}</span>
+                {open && (
+                  <div className='options'>
+                    {currentUser.isSeller && (
+                      <>
+                        <Link className='link' to='/mygigs'>
+                          Gigs
+                        </Link>
+                        <Link className='link' to='/add'>
+                          Add New Gig
+                        </Link>
+                      </>
+                    )}
+                    <Link className='link' to='/orders'>
+                    Orders
+                    </Link>
+                    <Link className='link' to='/messages'>
+                      Messages
+                    </Link>
+                    <button onClick={handleLogout}>
+                      Logout
+                    </button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <>
+                <Link to='/login' className='link'>Sign in</Link>
+                <Link className='link' to='/register'>
+                  <button>Join</button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
