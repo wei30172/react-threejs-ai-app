3cd96793d10f0e4ae5cac7f4f88d84587622e78a
claireWei <claire610323@gmail.com>
2023-06-01 16:52:42 -0700

    Login page

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -33,7 +33,7 @@ const Login: FC = () => {
       name: 'email',
       type: 'email',
       placeholder: 'Email',
-      pattern: '^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$',
+      pattern: '^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$',
       required: true,
       value: ''
     },
