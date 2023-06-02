0bb3f84ef7d24656080ae927fd624423d01ca3f3
claireWei <claire610323@gmail.com>
2023-06-01 20:54:53 -0700

    Eslint Fix

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,18 +6,18 @@ import './Home.scss'
 
 const Home: FC = () => {
   return (
-    <div className="home flex-center">
-      <div className="title">
-        <h1>CAPTURE <br className="break" /> THE MOMENT.</h1>
+    <div className='home flex-center'>
+      <div className='title'>
+        <h1>CAPTURE <br className='break' /> THE MOMENT.</h1>
       </div>
-      <div className="content">
+      <div className='content'>
         <p>Craft your personalized backdrop and photos with our advanced 3d customization features.
           Set your creativity free and establish your distinct aesthetic...
         </p>
-        <Link to="/add-gig">
+        <Link to='/add-gig'>
           <CustomButton
-            type="filled"
-            title="Design Your Own"
+            type='filled'
+            title='Design Your Own'
           />
         </Link>
       </div>
