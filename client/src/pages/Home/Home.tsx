ba14619dd0f55f392bfa94418818d251645df077
claireWei <claire610323@gmail.com>
2023-06-01 20:33:42 -0700

    Home page

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+
+import { CustomButton } from '../../components'
+import './Home.scss'
+
+const Home: FC = () => {
+  return (
+    <div className="home flex-center">
+      <div className="title">
+        <h1>CAPTURE <br className="break" /> THE MOMENT.</h1>
+      </div>
+      <div className="content">
+        <p>Craft your personalized backdrop and photos with our advanced 3d customization features.
+          Set your creativity free and establish your distinct aesthetic...
+        </p>
+        <Link to="/add-gig">
+          <CustomButton
+            type="filled"
+            title="Design Your Own"
+          />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
