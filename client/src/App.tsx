6ae750cf8c4164fa07e07886a894ac556854870a
claireWei <claire610323@gmail.com>
2023-06-02 14:53:24 -0700

    Install react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,21 @@
 import './styles/_main.scss'
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import { Home, Gigs, Gig, MyGigs, AddGig, Orders, Messages, Message, MyImages, AddImage, Register, Login } from './pages'
 import { Navbar, Footer } from './components'
 
 function App() {
   const Layout = () => {
+    const queryClient = new QueryClient()
+    
     return (
       <div className='app'>
-        <Navbar />
-        <Outlet />
-        <Footer />
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </QueryClientProvider>
       </div>
     )
   }
