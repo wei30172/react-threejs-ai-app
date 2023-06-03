822a3e50a4f9bb910b771e9a7260501b3e873969
claireWei <claire610323@gmail.com>
2023-06-03 15:44:28 -0700

    FilePicker Component

---
+++ client/src/components/customizer/filePicker/FilePicker.tsx
@@ -0,0 +1,47 @@
+import { FC, ChangeEvent } from 'react'
+
+import CustomButton from '../../button/CustomButton'
+import './FilePicker.scss'
+
+interface FilePickerProps {
+  setFile: (file: File) => void
+  readFile: (type: 'logo' | 'full') => void
+}
+
+const FilePicker: FC<FilePickerProps>  = ({ setFile, readFile }) => {
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    
+    if (files) {
+      setFile(files[0])
+    }
+  }
+
+  return (
+    <div className="file-picker glassmorphism glassmorphism-container">
+      <div className="top">
+        <input 
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+      </div>
+
+      <div className="bottom">
+        <CustomButton 
+          type="filled"
+          title="Logo"
+          handleClick={() => readFile('logo')}
+        />
+        <CustomButton 
+          type="filled"
+          title="Full"
+          handleClick={() => readFile('full')}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default FilePicker
