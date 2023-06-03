e90da03701406aff988beae30744568b2119bc1e
claireWei <claire610323@gmail.com>
2023-06-02 21:27:54 -0700

    GigCard Component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,11 +3,21 @@ import Footer from './footer/Footer'
 import FormInput from './form/FormInput'
 import CustomButton from './button/CustomButton'
 import Toast from './toast/Toast'
+import GigCard from './gigCard/GigCard'
+// import AIPicker from './aiPicker/AIPicker'
+// import ColorPicker from './colorPicker/ColorPicker'
+// import FilePicker from './filePicker/FilePicker'
+// import Tab from './tab/Tab'
 
 export {
   Navbar,
   Footer,
   FormInput,
   CustomButton,
-  Toast
+  Toast,
+  // AIPicker,
+  // ColorPicker,
+  // FilePicker,
+  // Tab,
+  GigCard
 }
