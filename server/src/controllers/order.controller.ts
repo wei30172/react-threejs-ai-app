34833b0ba00c3750540e0fb7c3c4b4b66cc30eb8
claireWei <claire610323@gmail.com>
2023-06-08 17:21:49 -0700

    Payment Api

--- server/src/controllers/order.controller.ts
+++ server/src/controllers/order.controller.ts
@@ -1,14 +1,48 @@
 import { NextFunction, Response } from 'express'
+import Stripe from 'stripe'
 import createError from '../utils/createError'
 import Order from '../models/order.model'
 import Gig from '../models/gig.model'
 import { IRequest } from '../middleware/jwt'
 
-export const createOrder =  async (req: IRequest, res: Response, next: NextFunction): Promise<void> => {
-  const gig = await Gig.findById(req.params.gigId)
+// export const createOrder =  async (req: IRequest, res: Response): Promise<void> => {
+//   const gig = await Gig.findById(req.params.gigId)
+
+//   if (!gig) throw createError(404, 'Gig not found')
+  
+//   const newOrder = new Order({
+//     gigId: gig._id,
+//     img: gig.cover,
+//     title: gig.title,
+//     buyerId: req.userId,
+//     sellerId: gig.userId,
+//     price: gig.price,
+//     payment_intent: 'test'
+//   })
+
+//   await newOrder.save()
+//   res.status(200).send('successful')
+// }
+
+export const intent = async (req: IRequest, res: Response, next: NextFunction): Promise<void> => {
+  const stripeKey = process.env.STRIPE_KEY
+  if (!stripeKey) {
+    return next(createError(500, 'Stripe key not set' )) 
+  }
+  const stripe = new Stripe(stripeKey, { apiVersion: '2022-11-15'})
+
+  const gig = await Gig.findById(req.params.id)
 
   if (!gig) throw createError(404, 'Gig not found')
-  
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: 'usd', // todo
+    automatic_payment_methods: {
+      enabled: true
+    }
+  })
+
   const newOrder = new Order({
     gigId: gig._id,
     img: gig.cover,
@@ -16,11 +50,14 @@ export const createOrder =  async (req: IRequest, res: Response, next: NextFunct
     buyerId: req.userId,
     sellerId: gig.userId,
     price: gig.price,
-    payment_intent: 'test'
+    payment_intent: paymentIntent.id
   })
 
   await newOrder.save()
-  res.status(200).send('successful')
+
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret
+  })
 }
 
 export const getOrders = async (req: IRequest, res: Response, next: NextFunction): Promise<void> => {
--- server/src/routes/order.route.ts
+++ server/src/routes/order.route.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express'
 import { verifyToken } from '../middleware/jwt'
-import { getOrders, confirm, createOrder } from '../controllers/order.controller'
+import { getOrders, intent, confirm } from '../controllers/order.controller'
+// import { getOrders, confirm, createOrder } from '../controllers/order.controller'
 
 const router: Router = express.Router()
 
-router.post('/:gigId', verifyToken, createOrder)
+// router.post('/:gigId', verifyToken, createOrder)
 router.get('/', verifyToken, getOrders)
+router.post('/create-payment-intent/:id', verifyToken, intent)
 router.put('/', verifyToken, confirm)
 
 export default router
