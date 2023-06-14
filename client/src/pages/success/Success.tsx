02f2cd1a4eecfcf6043e77d7e11b1c2f2b28b7a1
claireWei <claire610323@gmail.com>
2023-06-13 23:58:46 -0700

    Update Success Page

--- client/src/pages/success/Success.tsx
+++ client/src/pages/success/Success.tsx
@@ -1,5 +1,6 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+
 import newRequest from '../../utils/newRequest'
 import './Success.scss'
 
@@ -7,30 +8,39 @@ const Success: FC = () => {
   const navigate = useNavigate()
   const { search } = useLocation()
   const params = new URLSearchParams(search)
-  const payment_intent = params.get('payment_intent')
+  const paymentIntent = params.get('payment_intent')
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   useEffect(() => {
     const makeRequest = async () => {
+
+      if (!paymentIntent) {
+        setErrorMessage('Payment intent not found.')
+        return
+      }
+
       try {
-        await newRequest.put('/orders', { payment_intent })
+        await newRequest.put('/orders', { payment_intent: paymentIntent })
+      } catch (err) {
+        setErrorMessage('Failed to process the order.')
+      } finally {
         setTimeout(() => {
           navigate('/orders')
-        }, 5000)
-      } catch (err) {
-        console.log(err)
+        }, 3000)
       }
     }
 
-    if (payment_intent) {
-      makeRequest()
-    }
-  }, [navigate, payment_intent])
+    makeRequest()
+  }, [navigate, paymentIntent])
 
   return (
     <div className='success'>
       <div className='container'>
-        Payment successful. You are being redirected to the orders page. Please do
-        not close the page
+        {errorMessage 
+          ? <p>{errorMessage} You are being redirected to the orders page. Please do not close the page.</p> 
+          : <p>Payment successful. You are being redirected to the orders page. Please do not close the page.</p>
+        }
       </div>
     </div>
   )
